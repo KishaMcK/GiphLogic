7c9e983999ada9ce282ce52f8cb6b137da734740
KishaMcK <kisha.mckechnie@gmail.com>
2018-12-27 08:55:31 -0500

    html was completed

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    //create an array of logics
+    var logics = ["yes", "never", "yum", "omg", "again?", "not cool", "i got this", "you're not alone", "i love you", "merry christmas", "yea buddy", "i cant"];
+
+}
