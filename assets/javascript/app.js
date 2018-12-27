e57b6a9857fdc22b092b15b8817d37b3b33e41fa
KishaMcK <kisha.mckechnie@gmail.com>
2018-12-27 09:27:40 -0500

    Part of the JavaScript is finished, still not functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
     //create an array of logics
     var logics = ["yes", "never", "yum", "omg", "again?", "not cool", "i got this", "you're not alone", "i love you", "merry christmas", "yea buddy", "i cant"];
+    //function for displaying data//
+    function renderButtons() {
+        $("#logicView").empty();
+
+        for (var i = 0; i < logics.length; i++) {
+            var a = $("button");
+            a.addClass("logic");
+            a.attr("data-name", logics[i]);
+            a.text(logics[i]);
+            $("logicView").prepend(a);
+        }
+    }
+
+    //function button event//
+    $("addLogic").on("click", function () {
+        event.preventDefault();
+
+        var logicInput = $("#logicInput").val().trim();
+        logics.push(logicInput);
+        renderButtons();
+        $("#logicInput").val("");
+    });
 
 }
