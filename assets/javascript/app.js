0d56ea4dcbecdc413bcf2f79d333a5738112cb3e
KishaMcK <kisha.mckechnie@gmail.com>
2018-12-27 21:40:18 -0500

    JavaScript is not working, needs de-bugged

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,4 +24,52 @@ $(document).ready(function () {
         $("#logicInput").val("");
     });
 
-}
+    //giph display//
+    function showGiphs() {
+        var logicData = (this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + logicData + "&api_key=GlS46RxnXv9OrKm4aT0Y04198vPJ5GTX";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                var giphDiv = $("<div class='giph'>");
+                var showGiphs = $("<img>");
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                showGiphs.attr("src", results[i].images.fixed_height_still.url);
+
+                showGiphs.attr("title", "Rating:" + results[i].rating);
+                showGiphs.attr("datta-still", results[i].images.fixed_height_still.url);
+                showGiphs.attr("data-state", "still");
+                showGiphs.addClass("giph");
+                showGiphs.attr("data-animate", results[i].images.fixed_height.url);
+                giphDiv.append(p);
+                giphDiv.append(showGiphs);
+                $("#logicGiphs").prepend(giphDiv);
+            }
+        });
+    }
+
+    //animate + paus giphs//
+    $("#logicGiphs").on("click", ".giph", function (event) {
+        event.preventDefault();
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+
+    });
+
+    //show giphs//
+    $(document).on("click", ".logic", showGiphs);
+
+    renderButtons();
+});
